baa3a337ed4fb3a534c8cb69b540a900dd6bb0db
shenjunjian <40288193@qq.com>
2026-03-30 20:55:29 +0800

    fix:1

--- WebMcp/opentiny-webmcp/webmcp-remoter -client/src/App.vue
+++ WebMcp/opentiny-webmcp/webmcp-remoter -client/src/App.vue
@@ -1,18 +1,52 @@
 <script setup lang="ts">
-import { onMounted } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { onMounted, ref } from "vue";
 
 import { setupWebMcpServer, setupWebMcpClient } from "./webmcp";
+const currentTools = ref<ToolsType>([]);
 
-// APP中， 初始化一个内存通道的双方。
-// 1、其中client 负责连接到远端。
-// 2、其中server 负责不停的添加工具，详见 helloWorld组件。
+let webMcpClient: WebmcpClient;
+type ToolsType = Awaited<ReturnType<typeof webMcpClient.listTools>>["tools"];
+
+// 2.  刷新工具列表，做展示层UI
+function refreshTool() {
+  webMcpClient.listTools().then((tools) => {
+    console.log(tools);
+    currentTools.value = tools.tools;
+  });
+}
+
+// 3.  调用工具
+function callTool(tool: ToolsType[0]) {
+  webMcpClient.callTool({ name: tool.name, arguments: {} }).then((result) => {
+    alert(JSON.stringify(result));
+  });
+}
+
 onMounted(async () => {
-  const webMcpServer = await setupWebMcpServer();
-  const webMcpClient = await setupWebMcpClient();
+  // 1.  初始化客户端
+  webMcpClient = await setupWebMcpClient();
+  refreshTool();
 });
 </script>
 
 <template>
-  <HelloWorld />
+  <div class="tool-container">
+    <h3 @click="refreshTool">iframe:当前工具</h3>
+    <ul>
+      <li v-for="tool in currentTools" :key="tool.name" @click="callTool(tool)">
+        {{ tool.name }} - {{ tool.description }}
+      </li>
+    </ul>
+  </div>
 </template>
+
+<style lang="css" scoped>
+h3,
+li {
+  cursor: pointer;
+}
+
+li {
+  text-align: left;
+}
+</style>
