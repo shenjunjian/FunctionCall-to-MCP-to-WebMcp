b8e219a33983c1a6c479272ed327cda4e9a3315c
shenjunjian <40288193@qq.com>
2026-03-31 11:43:20 +0800

    fix: 1

--- WebMcp/opentiny-webmcp/webmcp-remoter -client/src/webmcp.ts
+++ WebMcp/opentiny-webmcp/webmcp-remoter -client/src/webmcp.ts
@@ -3,6 +3,8 @@ import { Client } from "@modelcontextprotocol/sdk/client/index";
 import { StreamableHTTPClientTransport } from "@modelcontextprotocol/sdk/client/streamableHttp";
 
 // 3、WebMcpServer受控端网页已经通过sessionId将自己代理给了`Web Agent`的远程服务器，所以此时我们可以通过sessionId来连接到远程进行间接的访问它了。
+// 由于 server端是一个标准mcpServer，已通过 web agent暴露出来。
+// 所以我们可以用opentiny的webmcpClient来连接到它， 也可以用标准的MCP CLIENT  来连接到它，甚至可以配置 mcpServer 的json 给 vscode, cursor, trae... 等工具调用它。
 
 // 3.1 使用opentiny的webmcpClient
 let webMcpClient: WebMcpClient | Client;
