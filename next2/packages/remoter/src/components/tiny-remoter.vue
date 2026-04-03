e3d6502992a7c1e652765ceb114635416271044b
shenjunjian <40288193@qq.com>
2026-04-03 20:52:39 +0800

    fix: 添加 remoter的目录

---
+++ next2/packages/remoter/src/components/tiny-remoter.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Hello Tiny Remoter</h1>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const count = ref(0);
+</script>
---
+++ next2/packages/remoter/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite-plus";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+  plugins: [vue()],
+  pack: {
+    dts: {
+      tsgo: true,
+    },
+    exports: true,
+  },
+  lint: {
+    options: {
+      typeAware: true,
+      typeCheck: true,
+    },
+  },
+  fmt: {},
+  staged: {
+    "*": "vp check --fix",
+  },
+});
