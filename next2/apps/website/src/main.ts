5b383e7fb4b10d28110c063ab94d11ac52c03b97
shenjunjian <40288193@qq.com>
2026-04-08 09:53:55 +0800

    对齐page server

--- next2/apps/website/src/main.ts
+++ next2/apps/website/src/main.ts
@@ -3,9 +3,8 @@ import { z } from "zod";
 
 import { Agent } from "agent";
 import { createDeepSeek } from "@ai-sdk/deepseek";
-  await registerOnPage({});
+await registerOnPage({});
 
- 
 window._z = z;
 
 const deepseek = createDeepSeek({
@@ -26,3 +25,25 @@ console.log(agent);
 
 window._agent = agent;
 // _agent.$mcpServers.mcpServers.value.push({type:'page', name:'xxxx 系统' })
+
+// navigator.modelContext.registerTool({
+//     name: "get-color",
+//     description: "获取当前颜色",
+//     inputSchema: { type: "object", properties: {} },
+//     async execute() {
+//       return {
+//         content: [
+//           {
+//             type: "text",
+//             text: `当前红色, from buildin-modelcontext`,
+//           },
+//         ],
+//       };
+//     },
+//   });
+// _agent.$mcpServers.mcpServers.value.push({type:'page', name:'xxxx 系统'})
+
+// _agent.chatStream({
+//   role: "user",
+//   content: "现在颜色是什么",
+// });
--- next2/packages/agent/src/hooks/useMcpServers.ts
+++ next2/packages/agent/src/hooks/useMcpServers.ts
@@ -1,15 +1,8 @@
 import { ref } from "vue";
 import type { NextMcpServer } from "../servers/servers";
-import {
-  McpServer,
-  type RegisteredTool,
-} from "@modelcontextprotocol/sdk/server/mcp";
-import { InMemoryTransport } from "@modelcontextprotocol/sdk/inMemory";
-import { Client } from "@modelcontextprotocol/sdk/client";
 import type { Agent } from "../agent";
 import type { ToolSet } from "ai";
-import { jsonSchemaToZod } from "json-schema-to-zod";
-import { z } from "zod";
+import { jsonSchema, tool } from "ai";
 /** 管理自定义的MCP服务 */
 export function useMcpServers(agent: Agent) {
   const mcpServers = ref<NextMcpServer[]>([]);
@@ -17,6 +10,7 @@ export function useMcpServers(agent: Agent) {
   // 最终传递给 ToolLoopAgent 的工具，不要修改引用地址
   const tools: ToolSet = {};
 
+  // 每次对话前，重新刷新所有工具
   agent.$lifeCycle.on("chatStart", async () => {
     // 清空工具
     Object.keys(tools).forEach((key) => {
@@ -34,9 +28,7 @@ export function useMcpServers(agent: Agent) {
 
   async function getToolsFromServer(server: NextMcpServer) {
     if (server.type === "page") {
-      // 从window.navigator 获取工具
-      const { server, client } = await createPagePair();
-      const clientTools =await client.listTools();  // TODO: 返回的是数组 {} mcp官方的， 不是ai-sdk所需要的
+      const clientTools = buildPageTools(); // TODO: 返回的是数组 {} mcp官方的， 不是ai-sdk所需要的
       console.log(clientTools, "本window页面的工具");
       return clientTools;
     }
@@ -53,67 +45,19 @@ export function useMcpServers(agent: Agent) {
   };
 }
 
- 
-
-/** 快速创建一个基于内存连接的MCPServer和MCPClient pair */
-export async function createPagePair() {
-  // createTransportPair();  备用方案，
-  const [clientTransport, serverTransport] =
-    InMemoryTransport.createLinkedPair();
-
-  const server = new McpServer(
-    { name: "web-mcp-server", version: "1.0.0" },
-    { capabilities: { tools: { listChanged: true } } }, // 必须允许 listChanged
-  );
-  proxyMcpServer(server);
-  await server.connect(serverTransport);
-
-  const client = new Client(
-    { name: "web-mcp-client", version: "1.0.0" },
-    {
-      capabilities: {
-        roots: { listChanged: true },
-      },
-    },
-  );
-
-  await client.connect(clientTransport);
-
-  return { server, client };
-}
-
-
-function refreshTools(server: McpServer) {
-  
-
+function buildPageTools() {
   const client = navigator.modelContextTesting!;
-
-  client.listTools().forEach((tool) => {
-    const zodSchema = jsonSchemaToZod(JSON.parse(tool.inputSchema as string),
-     { zodVersion: 3 } as any);
-    // 使用 new Function 将字符串转换为实际的 Zod 对象
-    const zodObject = new Function("z", `return ${zodSchema}`)(z);
-    
-    server.registerTool(
-      tool.name,
-      {
-        description: tool.description,
-        inputSchema: zodObject,
+  const tools: ToolSet = {};
+  client.listTools().forEach((currTool) => {
+    tools[currTool.name] = tool({
+      description: currTool.description,
+      inputSchema: jsonSchema(JSON.parse(currTool.inputSchema as string)),
+      // params是入参， aiContext 包含了 {toolCallId, messages,abortSignal}
+      execute: async (params, aiContext) => {
+        return client.executeTool(currTool.name, JSON.stringify(params)); // webmcp规范， 参数要字符串化。
       },
-      async (...args) => {
-        return client.executeTool(tool.name, ...args);
-      },
-    );
+    });
   });
-}
-/** 将当前页面的 McpServer 工具代理到 modelContextTesting 上
- *  这样当 McpClient 要执行工具时，实际转发给 modelContextTesting 去执行。
- */
-export function proxyMcpServer(server: McpServer) {
-  refreshTools(server);
 
-  //   const client = navigator.modelContextTesting!;
-  //   client.registerToolsChangedCallback(() => {
-  //     refreshTools(server);
-  //   });
+  return tools;
 }
